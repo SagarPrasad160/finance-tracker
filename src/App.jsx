c8b4e6e8ecdeff279a6c134d9d60aea1ae8b0560
SagarPrasad160 <sagarprasad916@gmail.com>
2023-05-25 17:57:24 +0530

    add pages,routes and Navbar

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
+import Navbar from "./components/Navbar";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
